f834c93b47676ca601e9066a66ed3182ffbe53cd
JPBM135 <jpedrobm0@gmail.com>
2025-03-17 01:02:26 -0300

    fix(publish): set error to strict

--- publish.sh
+++ publish.sh
@@ -1,3 +1,6 @@
+# Error handling
+set -e
+
 VERSION=v$(node -p -e "require('./package.json').version")
 
 # Check if the package version is already published
@@ -6,8 +9,18 @@ if git rev-parse $VERSION >/dev/null 2>&1; then
   exit 1
 fi
 
+yarn lint
+
+yarn test
+
 yarn build
+
 git add --all
+
 git commit -m "feat(release): release $VERSION" --allow-empty
+
 git tag $VERSION -m "feat(release): release $VERSION"
+
 git push --follow-tags
+
+echo "Version $VERSION published"
